03c3dda81d648ab64098418aabd6529a54683012
Alisson de Oliveira <alisson.oliveira2874@gmail.com>
2023-08-03 23:36:54 -0300

    Adicionada a aula de Documentação que demonstra o funcionamento dos variados estilos de comentários

---
+++ curso-java-basico/documentacao/src/Comentarios.java
@@ -0,0 +1,29 @@
+public class Comentarios {
+    // One Line
+
+    // Olá, eu sou um comentário em uma única linha
+
+    // Mult Line
+
+    /* Olá,
+     * Eu sou um comentario
+     * que posso ser mais detalhadod
+     * quando necessário
+     */
+
+    // Documentation
+
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+
+    // Javadoc - Gerador de documentação visual para navegação
+
+    // No terminal execute o comando abaixo
+
+    // javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java
+}
